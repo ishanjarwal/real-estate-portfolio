9f42957825d0db4e62c72d8e24738f34b77eb433
ishanjarwal <ishan.professional01@gmail.com>
2024-06-02 19:56:54 +0530

    lenis scroll added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -8,8 +8,28 @@ import About from './sections/About'
 import Features from './sections/Features'
 import StartNow from './sections/StartNow'
 import Contact from './sections/Contact'
+import Lenis from '@studio-freight/lenis';
 
 const App = () => {
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 0.2, // Lowered the duration for faster scrolling
+      easing: (t) => t, // Linear easing function for a consistent speed
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <main className=''>
       <Header />
